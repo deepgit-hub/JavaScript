fe9bc647526174c0a31b9d40e2604c099b831e12
Deepak <darkdeepak001@gmail.com>
2026-05-11 16:33:17 +0530

    Add functionality to sum two numbers and print results
    Implement addition of two numbers with console output.

---
+++ 02.js
@@ -0,0 +1,16 @@
+❓ Question 2: Addition of Two Numbers
+
+👉 Create:
+
+Two variables storing numbers
+Another variable to store their sum
+
+Print:
+
+First number
+Second number
+Result
+
+using:
+
+console.log()
