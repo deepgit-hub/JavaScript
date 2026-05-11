bfef063f6aaf5aa3db3bf8e338b240b0a18a5983
Deepak <darkdeepak001@gmail.com>
2026-05-11 16:31:26 +0530

    Store and print personal information
    Added variables for personal information and printed them.

--- 01.js
+++ 01.js
@@ -1,3 +1,14 @@
+❓ Question 1: Print Personal Information
+
+👉 Create variables to store:
+
+Your name
+Your age
+Your college name
+
+Print all values using:
+
+console.log()
 var name = "Deepak"
 var age = 20
 var college ="DBC International"
