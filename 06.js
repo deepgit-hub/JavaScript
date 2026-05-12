71c97fc1a2e38b76bfc9e6afb97aa7c03d68a4da
Deepak <darkdeepak001@gmail.com>
2026-05-12 16:06:31 +0530

    Add script to calculate and display marks and average

---
+++ 06.js
@@ -0,0 +1,10 @@
+var math = 95
+var science = 60
+var eng = 35
+var avg = (math+science+eng)/3
+var total = math + science + eng
+console.log("Mark in Maths : "+math)
+console.log("Mark in Science : "+science)
+console.log("Mark in English : "+eng)
+console.log("Total Mark "+total)
+console.log("Average "+avg)
