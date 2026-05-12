d5331f0a32be9739b888919294e44e3607c7e566
Deepak <darkdeepak001@gmail.com>
2026-05-12 16:26:28 +0530

    Add cost calculation for units used

---
+++ 09.js
@@ -0,0 +1,8 @@
+var name = "Deepak"
+var units_used = 356
+var cost_per_unit = 8
+var amount = units_used * cost_per_unit
+var total_amount = amount + (amount*5)/100
+console.log("Units used "+units_used)
+console.log("Cost per unit "+cost_per_unit)
+console.log("Total Amount : "+total_amount)
