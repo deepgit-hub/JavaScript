9d9ee9fe343ae57fc356f85bd42b7b3658320a05
Deepak <darkdeepak001@gmail.com>
2026-05-12 16:34:07 +0530

    Initialize shopping bill variables and total calculation
    Calculate total price for shopping bill and set up variables.

--- 10.js
+++ 10.js
@@ -1,5 +1,23 @@
 /*
+❓ Question 10: Shopping Bill System
 
+👉 Create variables for:
+
+Product name
+Product price
+Quantity
+
+Tasks:
+
+Calculate total price
+Apply 10% discount
+Find final payable amount
+
+Formula:
+
+Final Amount=(Price×Quantity)−10% Discount
+
+Print all details clearly
 */var product_name = "Hair wash"
 var price  = 400
 var q = 2
