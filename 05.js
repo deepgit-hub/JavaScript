13178be545a52045e8b8535119bad8da3d322981
Deepak <darkdeepak001@gmail.com>
2026-05-12 11:42:32 +0530

    Add student information logging in 05.js

---
+++ 05.js
@@ -0,0 +1,8 @@
+var name  = "Deepak"
+var dep ="Computer Science"
+var college = "Don Bosco College"
+var cgpa = 6
+console.log("Student Name : "+name)
+console.log("Department   : "+dep)
+console.log("College      : "+college)
+console.log("CGPA         : "+cgpa)
