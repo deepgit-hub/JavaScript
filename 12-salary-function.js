c2e93f81860799b013b938a84421f733415ed3b7
Deepak <darkdeepak001@gmail.com>
2026-05-13 03:06:05 +0530

    Add salary function for employee salary calculation
    Implement salary function to calculate total salary.

--- 12-salary-function.js
+++ 12-salary-function.js
@@ -1,3 +1,17 @@
+/*
+❓ Question 12: Employee Salary Calculator Function
+
+Create a JavaScript function named salary() that takes an employee's name,
+basic salary, and
+bonus as parameters. 
+The function should calculate the total salary by adding the salary and bonus together,
+print the employee name and total salary using console.log(),
+and finally return the total salary.
+
+Formula:
+
+Total Salary=Salary+Bonus
+*/
 function salary(name,salary,bonus)
 {
     console.log("Name of the Employee : "+name)
