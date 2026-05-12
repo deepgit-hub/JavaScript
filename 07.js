5829f727c7198a036f7ef519e8602a17b654092d
Deepak <darkdeepak001@gmail.com>
2026-05-12 16:14:50 +0530

    Add simple interest calculation in 07.js

---
+++ 07.js
@@ -0,0 +1,9 @@
+var principle_amount = 20000
+var intrest = 5
+var years =2
+var simple_intrest = (principle_amount*intrest*years)/100
+console.log("Principle Amount : "+principle_amount)
+console.log("Intrest : "+intrest)
+console.log("Total Years : "+years)
+console.log("Simple Intrest Amount "+simple_intrest)
+console.log("Total Amount : "+(principle_amount+simple_intrest))
