8c8bd81ef59ca92c6e57dc5985272a2e6b1c2a84
Deepak <darkdeepak001@gmail.com>
2026-05-12 16:20:44 +0530

    Implement salary calculation system
    Added variables and calculations for salary components.

--- 08.js
+++ 08.js
@@ -1,3 +1,24 @@
+/*
+❓ Question 08: Salary Calculation System
+
+👉 Create variables for:
+
+Employee name
+Basic salary
+
+Tasks:
+
+Calculate:
+HRA = 20% of basic salary
+Bonus = 10% of basic salary
+Find total salary
+
+Formula:
+
+Total Salary=Basic Salary+HRA+Bonus
+
+Print all values clearly
+*/
 var emp_name = "Deepak"
 var basic_salary = 20000
 var hra = (basic_salary*20)/100
