16a921d4e0537d8aa73d537e23f792b36e2d034c
Deepak <darkdeepak001@gmail.com>
2026-05-13 02:20:51 +0530

    Initialize student variables and log types

---
+++ 11.js
@@ -0,0 +1,10 @@
+var student_name = "Deepak"
+var age = 19
+var attendence = false
+var address = null
+var phone_number 
+console.log(typeof(student_name))
+console.log(typeof(age))
+console.log(typeof(address))
+console.log(typeof(attendence))
+console.log(typeof(phonenumber))
