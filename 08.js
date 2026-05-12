82e2e60c058d21923fce8ccd3847e46fac8fff0a
Deepak <darkdeepak001@gmail.com>
2026-05-12 16:20:06 +0530

    Add employee salary calculation in 08.js

---
+++ 08.js
@@ -0,0 +1,7 @@
+var emp_name = "Deepak"
+var basic_salary = 20000
+var hra = (basic_salary*20)/100
+var bonus = (basic_salary*10)/100
+var total_salary = basic_salary+hra+bonus
+console.log("Name "+emp_name)
+console.log("Total Salary "+total_salary)
