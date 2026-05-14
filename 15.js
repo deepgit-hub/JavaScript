51bdeec21a9c6728b214aafcb8e21ecb11983793
Deepak <darkdeepak001@gmail.com>
2026-05-14 11:20:04 +0530

    Add movieticket function for booking details

---
+++ 15.js
@@ -0,0 +1,10 @@
+function movieticket(name,movie,price,count)
+{
+    console.log("----Your Booking Details----")
+    console.log("Name                : "+name)
+    console.log("Movie               : "+movie)
+    console.log("Price               : "+price)
+    console.log("Number of tickets   : "+count)
+    console.log("Total amount to pay : "+(price*count))
+}
+movieticket("Deepak","Openheimer",1200,3)
