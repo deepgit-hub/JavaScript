5c3b95334287c1c169504f8144f711753a699c04
Deepak <darkdeepak001@gmail.com>
2026-05-14 11:26:02 +0530

    Add BMI calculator function in 15.js

---
+++ 15.js
@@ -0,0 +1,9 @@
+function bmicalculator(name,w,h)
+{
+    console.log("Name      : "+name)
+    console.log("Weight    : "+w+"kg")
+    console.log("Height    : "+h+"m")
+    var bmi = w/(h*h)
+    console.log("BMI value : "+bmi)
+}
+bmicalculator("Chandra seker",92,1.5)
