bb02db52f1ba1ff06a3cd291eff65ce7d723356d
Deepak <darkdeepak001@gmail.com>
2026-05-15 16:51:49 +0530

    Add examHallPermission function for exam eligibility
    Implemented the examHallPermission function with eligibility checks based on attendance, exam fee status, and ID card availability.

--- 18-exam-hall-system.js
+++ 18-exam-hall-system.js
@@ -1,4 +1,41 @@
-/**/
+/*
+❓ Question 18: College Login & Exam Hall Permission System
+
+Create a JavaScript function named examHallPermission() that takes:
+
+student name
+register number
+attendance percentage
+exam fee paid status (true or false)
+ID card availability (true or false)
+
+The system should follow these rules:
+
+First check attendance:
+
+If attendance is below 75:
+Print "Not Eligible Due to Low Attendance"
+
+If attendance is valid:
+
+Check exam fee status:
+If fee not paid:
+
+Print "Exam Fee Pending"
+
+If fee is paid:
+
+Check ID card:
+
+If ID card not available:
+
+Print "ID Card Required"
+
+If all conditions are satisfied:
+Print:
+student details
+"Allowed to Enter Exam Hall"
+*/
 function examhallpermission(name,roll_no,attendence,exam_fee,id)
 {
     if(attendence>75)
