046164121c500f13062e0de2231f592074e38cd4
Deepak <darkdeepak001@gmail.com>
2026-05-16 13:25:32 +0530

    Create foodOrder function for food ordering
    Implement foodOrder function for online food ordering system with discount logic and wallet balance check.

--- 19.js
+++ 19.js
@@ -1,3 +1,38 @@
+❓ Question 19: Online Food Order System
+
+Create a JavaScript function named foodOrder() that takes:
+
+customer name
+food item
+quantity
+price per item
+wallet balance
+premium membership status (true or false)
+
+The system should follow these rules:
+
+First calculate total food cost.
+
+Formula:
+
+Total Cost=Quantity×Price Per Item
+
+If customer is a premium member:
+Give 20% discount.
+Otherwise:
+Give 5% discount.
+After discount:
+Check wallet balance.
+If wallet balance is insufficient:
+Print "Insufficient Wallet Balance"
+Else:
+Deduct amount from wallet
+Print:
+customer details
+food item
+final amount
+remaining wallet balance
+"Order Placed Successfully"
 function foodOrder(name,food,qauntity,price,wallet,membership)
 {
     var total = qauntity * price
