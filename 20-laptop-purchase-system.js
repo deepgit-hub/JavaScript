a720c6f5df57d3cd1d8f8c09c13347345d45441c
Deepak <darkdeepak001@gmail.com>
2026-05-17 09:46:24 +0530

    Add laptopPurchase function for purchase eligibility
    Implemented laptopPurchase function to determine eligibility for purchasing a laptop based on budget, student status, and loan eligibility.

--- 20-laptop-purchase-system.js
+++ 20-laptop-purchase-system.js
@@ -1,4 +1,35 @@
-/**/function laptopPurchase(name,price,budget,student,loan)
+/*
+❓ Question 20: Laptop Purchase Eligibility System
+
+Create a JavaScript function named laptopPurchase() that takes:
+
+customer name
+laptop price
+customer budget
+student status (true or false)
+bank loan eligibility (true or false)
+
+The system should follow these rules:
+
+If customer budget is greater than or equal to laptop price:
+Print "Laptop Purchase Successful"
+Otherwise:
+Check if customer is a student:
+Students get 10% discount.
+After discount:
+Check whether budget is enough.
+If still not enough:
+Check bank loan eligibility:
+If eligible:
+Print "Laptop Can Be Purchased Using Loan"
+Else:
+Print "Purchase Failed"
+If purchase succeeds:
+Print:
+customer name
+final laptop price
+remaining balance
+*/function laptopPurchase(name,price,budget,student,loan)
 {
     if(student==true)
     {
