0f2aca45bf081c45f003f19aa48b0f9100fae345
Deepak <darkdeepak001@gmail.com>
2026-05-17 09:44:37 +0530

    Add laptopPurchase function for budget management

---
+++ 20.js
@@ -0,0 +1,24 @@
+function laptopPurchase(name,price,budget,student,loan)
+{
+    if(student==true)
+    {
+        price -= price*(10/100)
+    }
+    if(price<=budget)
+    {
+        console.log("Purchased Successfully")
+        budget = budget-price
+        console.log("Remaining Budget : "+budget)
+    }
+    else
+    {
+        if(loan==true)
+        {
+            console.log("Laptop can be purchased through loan!!!")
+        }
+        else{
+            console.log("Laptop purchase failed!!!")
+        }
+    }
+}
+laptopPurchase("Deepak",200000,150000,true,true)
