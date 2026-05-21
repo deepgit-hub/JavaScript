1fbb716c5ee442f803c072ca0178952d0ff507b3
Deepak <darkdeepak001@gmail.com>
2026-05-22 02:40:37 +0530

    Implement login system with validation checks

--- 21.js
+++ 21.js
@@ -25,3 +25,29 @@ If everything is correct:
 Print "Login Successful"
 
 */
+
+function loginsystem(u_name,password,otp_verification,account_status)
+{
+   let correct_username ="Deepak"
+   let correct_password =2911
+   let correct_otp = 7485
+   let correct_account_status = true
+   if(u_name == correct_username && password == correct_password && otp_verification == correct_otp && correct_account_status==account_status)
+   {
+        console.log("Login Successfully!!!")
+   }
+   else if(u_name != correct_username || password != correct_password)
+   {
+        console.log("Invalid Credentials entered!!!")
+   }
+   else if(!account_status)
+   {
+        console.log("Your account is Blocked!!!")
+   }
+   else if(otp_verification != correct_otp)
+   {
+        console.log("Otp is not verified!!!")
+   }
+
+}
+loginsystem("Deepak",2911,7485,true)
