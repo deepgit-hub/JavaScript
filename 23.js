c4774c465367e502d0d654d008730fb64f8a10b2
Deepak <darkdeepak001@gmail.com>
2026-05-23 02:04:25 +0530

    Add roll number generator function

---
+++ 23.js
@@ -0,0 +1,12 @@
+function rollnumbergenerator(start,end)
+{
+     var a=1
+     for(i=start;i<=end;i++)
+     {
+          
+          console.log("Student "+a+" roll number is : "+i)
+          a++;
+
+     }
+}
+rollnumbergenerator(101,155)
