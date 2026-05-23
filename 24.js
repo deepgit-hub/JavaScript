2dbbab3d2a1c871f7be8fa829b2dfd05a86616bb
Deepak <darkdeepak001@gmail.com>
2026-05-23 17:26:40 +0530

    Add HTML and JavaScript for welcome message

---
+++ 24.js
@@ -0,0 +1,15 @@
+<html>
+    <body>
+        <h1 id="title">Welcome</h1>
+        <button onclick="change()" id="but">Click me</button>
+    </body>
+</html>
+<script>
+    function change()
+    {
+        var h = document.getElementById("title")
+        h.textContent = "Welcome DEEPAK!!!"
+        
+        
+    }
+</script>
