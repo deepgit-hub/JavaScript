a073377792f59596f69d2a72f1f8c6cabb7eace9
Deepak <darkdeepak001@gmail.com>
2026-05-23 17:37:15 +0530

    Create 26.js

---
+++ 26.js
@@ -0,0 +1,21 @@
+<html>
+    <body>
+        <h1 id="title">Light is OFF!!!</h1>
+        <button onclick="on()" id="but">ON</button>
+        <button onclick="off()" id="but">OFF</button>
+    </body>
+</html>
+<script>
+    function on()
+    {
+        var h = document.getElementById("title")
+        h.textContent = "Light is ON!!!"
+        console.log(h.textContent)   
+    }
+    function off()
+    {
+        var h = document.getElementById("title")
+        h.textContent = "Light is OFF!!!"
+        console.log(h.textContent)
+    }
+</script>
