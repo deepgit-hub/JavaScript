2e632812cb441d0ca433ec4ff53b269c20674177
Deepak <darkdeepak001@gmail.com>
2026-05-24 16:39:19 +0530

    Create 29.js

---
+++ 29.js
@@ -0,0 +1,12 @@
+<html>
+    <body>
+        <h1 onmouseover="change()" id="title">Safe Zone</h1>
+    </body>
+</html>
+<script>
+    function change()
+    {
+        var h = document.getElementById("title")
+        h.textContent="Danger zone"
+    }
+</script>
