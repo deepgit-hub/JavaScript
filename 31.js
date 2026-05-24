e5b421ccc5f0dd7a092899fae539bba8f4c813d5
Deepak <darkdeepak001@gmail.com>
2026-05-24 16:46:44 +0530

    Create 31.js

---
+++ 31.js
@@ -0,0 +1,17 @@
+<html>
+    <body>
+        <h1 onmouseover="change()" onmouseout="notchange()" id="title">System Idle</h1>
+    </body>
+</html>
+<script>
+    function change()
+    {
+        var h = document.getElementById("title")
+        h.textContent="System Active"
+    }
+    function notchange()
+    {
+        var h = document.getElementById("title")
+        h.textContent="System Idle"
+    }
+</script>
