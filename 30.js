205108e611219cfefc6573dfb51462bdbb080c4c
Deepak <darkdeepak001@gmail.com>
2026-05-24 16:41:11 +0530

    Create 30.js

---
+++ 30.js
@@ -0,0 +1,12 @@
+<html>
+    <body>
+        <h1 onmouseover="change()" id="title">Movie Name?</h1>
+    </body>
+</html>
+<script>
+    function change()
+    {
+        var h = document.getElementById("title")
+        h.textContent="F1"
+    }
+</script>
