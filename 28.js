329f0ebe76fe6970806edcf085749cb0b8dd41da
Deepak <darkdeepak001@gmail.com>
2026-05-24 10:29:46 +0530

    Add HTML and JavaScript for user login/logout

---
+++ 28.js
@@ -0,0 +1,23 @@
+<html>
+    <body>
+        <h1 id="title">Normal User</h1>
+        <button onclick="lin()" id="but">log in</button>
+        <button onclick="out()" id="but">log out</button>
+        
+    </body>
+</html>
+<script>
+    function lin()
+    {
+        let h = document.getElementById("title")
+        h.textContent="Ethical Hacker"
+        console.log("User logged in")
+    }
+    function out()
+    {
+        let h = document.getElementById("title")
+        h.textContent ="Normal User"
+        console.log("User Logged out")
+    }
+    
+</script>
