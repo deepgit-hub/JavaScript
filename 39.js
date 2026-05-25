ac1852b10db23ef9d78cc1c06042c2dbfa4c6bf1
Deepak <darkdeepak001@gmail.com>
2026-05-26 03:19:07 +0530

    Update 39.js

--- 39.js
+++ 39.js
@@ -10,3 +10,24 @@ When button clicked:
 
 add both numbers
 display result
+<html>
+    <body>
+       <input id="num1">
+       <input id="num2">
+       <button id="button" onclick="add()">Add</button>
+       <h1 id="result">Result : </h1>
+
+
+    </body>
+</html>
+<script>
+    function add()
+    {
+        var a = document.getElementById("num1").value
+        var b = document.getElementById("num2").value
+        var total = Number(a)+Number(b)
+        var result = document.getElementById("result")
+        result.textContent ="Result : "+total
+        
+    }
+</script>
