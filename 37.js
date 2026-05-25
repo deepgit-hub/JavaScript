75f3fe9e3d3f4251c05e4290a38680c43f8b4a64
Deepak <darkdeepak001@gmail.com>
2026-05-25 11:06:03 +0530

    Update 37.js

--- 37.js
+++ 37.js
@@ -6,3 +6,24 @@ When page opens:
 Welcome Deepak
 
 should appear automatically.
+
+  <html>
+
+<body onload="welcome()">
+
+<h1 id="result"></h1>
+
+<script>
+
+function welcome()
+{
+    var result = document.getElementById("result")
+
+    result.textContent = "Welcome Deepak"
+}
+
+</script>
+
+</body>
+
+</html>
