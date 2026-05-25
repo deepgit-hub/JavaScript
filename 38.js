cec46050a6fcbe9e6a694e3b490217b1eed3b25c
Deepak <darkdeepak001@gmail.com>
2026-05-25 11:09:31 +0530

    Update 38.js

--- 38.js
+++ 38.js
@@ -6,3 +6,20 @@ When page loads:
 System Starting...
 
 should display automatically. */
+
+<html>
+    <body onload="welcome()">
+       
+        <h1 id="hello" ></h1>
+
+    </body>
+</html>
+<script>
+    function welcome()
+    {
+        var i = document.getElementById("hello")
+        i.textContent = "System Starting!!!"
+        
+    }
+    
+</script>
