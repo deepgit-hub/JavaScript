ed64dcd37b59574e5bbb527f694afbd67a84b001
Deepak <darkdeepak001@gmail.com>
2026-05-25 11:01:20 +0530

    Create 36.js

---
+++ 36.js
@@ -0,0 +1,16 @@
+<html>
+    <body>
+       <input id="input" onkeydown="hack()" >
+        <h1 id="result" >System Safe</h1>
+
+    </body>
+</html>
+<script>
+    function hack()
+    {
+        var i = document.getElementById("result")
+        i.textContent = "Accessing System"
+        
+    }
+    
+</script>
