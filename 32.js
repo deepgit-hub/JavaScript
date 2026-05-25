17e055b864536a8f3900d6df0926f0d1d66fa795
Deepak <darkdeepak001@gmail.com>
2026-05-25 08:26:11 +0530

    Create 32.js

---
+++ 32.js
@@ -0,0 +1,20 @@
+<html>
+    <body>
+       
+        <h1 id="result" onmouseover="on()" onmouseout="off()">Light off</h1>
+
+    </body>
+</html>
+<script>
+    function on()
+    {
+        var r = document.getElementById("result")
+        r.textContent = "Light on"
+    }
+     function off()
+    {
+        var r = document.getElementById("result")
+        r.textContent = "Light off"
+    }
+    
+</script>
