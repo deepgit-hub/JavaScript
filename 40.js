39158cb1e41410ffd0cc9d5b82f1acf183298af6
Deepak <darkdeepak001@gmail.com>
2026-05-29 03:42:11 +0530

    Add live heading update feature with input box

--- 40.js
+++ 40.js
@@ -7,3 +7,19 @@ one heading
 
 When user types inside input box,
 the heading should update LIVE
+<input id="text" onkeyup="change()">
+
+<h1 id="result"></h1>
+
+<script>
+
+    var textBox = document.getElementById("text")
+
+    var result = document.getElementById("result")
+
+    function change()
+    {
+        result.textContent = textBox.value
+    }
+
+</script>
