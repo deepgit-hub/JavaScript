e7334d469b337d032931da0132a2cea8c7bfd813
Deepak <darkdeepak001@gmail.com>
2026-05-29 04:03:22 +0530

    Add input and button for user name display
    Implement input and button to display user name.

---
+++ 42.js
@@ -0,0 +1,19 @@
+🎯 Question 42 (Next Challenge)
+
+Create:
+
+<input id="name">
+
+<button>Add Name</button>
+
+When user types:
+
+Deepak
+
+and clicks button,
+
+output:
+
+<h1>Deepak</h1>
+
+inside the div.
