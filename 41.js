ef9a2649fa3e144f7bad0a932aa94bbd93396a3b
Deepak <darkdeepak001@gmail.com>
2026-05-29 04:02:34 +0530

    Add button to create and display 'Hello' element

--- 41.js
+++ 41.js
@@ -6,3 +6,18 @@ textContent
 onclick
 DOM Manipulation
 */
+<button onclick="add()">Add Hello</button>
+<div id="result" > 
+
+    
+</div>
+
+<script>
+    var r = document.getElementById("result")
+    function add()
+    {
+        var h = document.createElement("h1")
+        h.textContent="Hello"
+        r.append(h)
+    }
+</script>
