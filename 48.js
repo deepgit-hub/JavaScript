3a543a13c939695492a921ac3a8f54d814e43ec5
Deepak <darkdeepak001@gmail.com>
2026-06-02 04:52:52 +0530

    Create button to add heading using innerHTML
    Implement button to add heading to div on click.

---
+++ 48.js
@@ -0,0 +1,18 @@
+.
+
+📂 Question 48 — Add Heading Using innerHTML
+🎯 Task
+
+Create:
+
+<button>Add Heading</button>
+
+<div id="result"></div>
+
+When button is clicked:
+
+Output:
+
+<h1>Hello Deepak</h1>
+
+should appear inside the div.
