424451fae6c3c3f7b5e0b79d5eb7c1fe1701132d
Deepak <darkdeepak001@gmail.com>
2026-06-01 22:40:39 +0530

    Add buttons to update greeting message

--- 45.js
+++ 45.js
@@ -1,1 +1,13 @@
+<h1 id="result">Name</h1>
+<button  onclick="update(event)">Deepak</button>
+<button  onclick="update(event)">Kathir</button>
+<button  onclick="update(event)">Antony Kevin</button>
+<script>
+    var r = document.getElementById("result")
+    var btn = document.getElementById("btn")
+    function update(event)
+    {
+        r.textContent="Hello Mr "+event.target.textContent
+    }
+</script>
 
