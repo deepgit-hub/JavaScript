af53009a728172ea6534418b9408b19d1b4dbc22
Deepak <darkdeepak001@gmail.com>
2026-06-02 04:39:52 +0530

    Add HTML with clickable headings to remove them

---
+++ 47.js
@@ -0,0 +1,15 @@
+<html>
+    <h1 onclick="deleteh(event)">One</h1>
+    <h1 onclick="deleteh(event)">Two</h1>
+    <h1 onclick="deleteh(event)">Three</h1>
+    <h1 onclick="deleteh(event)">Four</h1>
+    <h1 onclick="deleteh(event)">Five</h1>
+    <h1 onclick="deleteh(event)">Six</h1>
+
+</html>
+<script>
+    function deleteh(event)
+    {
+        event.target.remove()
+    }
+</script>
