436d716f103c71f44ed09c7339c1be2ab11ab689
Deepak <darkdeepak001@gmail.com>
2026-06-02 05:03:01 +0530

    Add button to display names in div

---
+++ 49.js
@@ -0,0 +1,13 @@
+<div id="d">
+
+</div>
+<button onclick="addh()">Add Name</button>
+<script>
+    var di = document.getElementById("d")
+    function addh()
+    {
+    di.innerHTML="<h1>Deepak</h1>"
+    di.innerHTML+="<h1>kathir</h1>"
+    di.innerHTML+="<h1>Antony Kevin</h1>"
+    }
+</script>
