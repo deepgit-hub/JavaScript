c1652bfe3dd0e65fd9d324e1555b5a0e3775dfc9
Deepak <darkdeepak001@gmail.com>
2026-06-02 07:53:19 +0530

    Implement button to change status from Offline to Online
    Added HTML structure and button functionality to change text from 'Offline' to 'Online'.

--- 52-id-selector.js
+++ 52-id-selector.js
@@ -1,4 +1,25 @@
-/**/
+/*
+📂 Question 52 — getElementById()
+🎯 Task
+
+Create:
+
+<h1 id="result">Offline</h1>
+
+<button>Connect</button>
+
+When button clicked:
+
+Offline
+
+becomes:
+
+Online
+
+Use:
+
+document.getElementById()
+*/
 <h1 id="h">Offline</h1>
 <button onclick="change()">Change</button>
 <script>
