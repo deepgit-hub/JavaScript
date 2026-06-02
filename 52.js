959bffcda9e85fa5ad8a07528f8671187f13cc1e
Deepak <darkdeepak001@gmail.com>
2026-06-02 07:51:29 +0530

    Add offline/online toggle functionality

---
+++ 52.js
@@ -0,0 +1,9 @@
+<h1 id="h">Offline</h1>
+<button onclick="change()">Change</button>
+<script>
+    var a = document.getElementById("h")
+    function change()
+    {
+        a.textContent="Online"
+    }
+</script>
