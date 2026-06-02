177a9ee11711539f649a9d056979f40041b39289
Deepak <darkdeepak001@gmail.com>
2026-06-03 01:20:44 +0530

    Added Question for 55

---
+++ 55.js
@@ -0,0 +1,18 @@
+📂 Question 55 — Insert Before Element
+🎯 Task
+
+Create:
+
+<div id="box">
+    <h1>Cyber Security</h1>
+</div>
+
+<button>Revel</button>
+
+When button clicked:
+
+<h1>My aim is </h1>
+
+<div id="box">
+    <h1>Cyber Security</h1>
+</div>
