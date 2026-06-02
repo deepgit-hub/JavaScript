2eea030246101579477b1b5ee2c22de4cabaf400
Deepak <darkdeepak001@gmail.com>
2026-06-02 07:23:55 +0530

    Add HTML with button to change title text

---
+++ 50.js
@@ -0,0 +1,10 @@
+<h1 class="title">Hello</h1>
+<button onclick="change()">Click ME!!!
+</button>
+<script>
+    var t = document.querySelector(".title")
+    function change()
+    {
+        t.textContent="Welcome Deepak!!!"
+    }
+</script>
