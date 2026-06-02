c58f6c72bd8d6055a4636c0d70e9c31fe6bacb53
Deepak <darkdeepak001@gmail.com>
2026-06-02 07:39:30 +0530

    Add HTML with button to change headings

---
+++ 51.js
@@ -0,0 +1,13 @@
+<h1>Java</h1>
+<h1>Python</h1>
+<h1>JavaScript</h1>
+<button onclick="change()">Change</button>
+<script>
+    var a = document.querySelectorAll("h1")
+    function change()
+    {
+        a[0].textContent="Programming Language"
+        a[1].textContent=""
+        a[2].textContent=""
+    }
+</script>
