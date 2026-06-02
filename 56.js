4d25dab6395fc8925bc81ca99095adc0cc26e448
Deepak <darkdeepak001@gmail.com>
2026-06-03 01:23:30 +0530

    Create HTML structure for button and headings
    Add HTML structure for a button and headings.

---
+++ 56.js
@@ -0,0 +1,18 @@
+📂 Question 56 — Insert After Element
+🎯 Task
+
+Create:
+
+<div id="box">
+    <h1>Frontend Developer</h1>
+</div>
+
+<button>Revel Next Goal</button>
+
+When button clicked:
+
+<div id="box">
+    <h1>Frontend Developer</h1>
+</div>
+
+<h1>Cyber Security Expert</h1>
