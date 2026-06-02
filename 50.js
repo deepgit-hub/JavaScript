34f8506d4fbfadccfec01f30011e6bb62b0ab4d8
Deepak <darkdeepak001@gmail.com>
2026-06-02 07:27:04 +0530

    Implement title change functionality with button
    Added HTML structure with a button to change the title text.

--- 50.js
+++ 50.js
@@ -1,4 +1,25 @@
-/**/
+/*
+📂 Question 50 — querySelector()
+🎯 Task
+
+Create:
+
+<h1 class="title">Hello</h1>
+
+<button>Change Text</button>
+
+When button clicked:
+
+Hello
+
+becomes:
+
+Welcome Deepak
+
+Use:
+
+document.querySelector(".title")
+*/
 <h1 class="title">Hello</h1>
 <button onclick="change()">Click ME!!!
 </button>
