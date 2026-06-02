1b543620f522bcc8a0151272d5d211907b41e106
Deepak <darkdeepak001@gmail.com>
2026-06-03 01:24:41 +0530

    Add button to reveal message before element

--- 55-insert-before-element.js
+++ 55-insert-before-element.js
@@ -17,3 +17,16 @@ When button clicked:
     <h1>Cyber Security</h1>
 </div>
 */
+<div id="box">
+    <h1>Cyber Security</h1>
+    <button onclick="revel()">Revel</button>
+</div>
+<script>
+    var a = document.querySelector("#box")
+    function revel()
+    {
+    var h = document.createElement("h1")
+    h.textContent="My AIM is to become"
+    a.insertAdjacentElement("beforebegin",h)
+    }
+</script>
