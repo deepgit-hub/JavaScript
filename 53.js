a1837385f4f804ad6a123f15c4a95286835db1be
Deepak <darkdeepak001@gmail.com>
2026-06-02 19:02:40 +0530

    Add HTML file with student names and highlight button

---
+++ 53.js
@@ -0,0 +1,17 @@
+<html>
+    <h1 class="student">Deepak</h1>
+    <h1 class="student">Kathir</h1>
+    <h1 class="student">Antony Kevin</h1>
+    <button onclick="change()">Highlight First</button>
+    <script>
+        function change()
+        {
+            var r=document.getElementsByClassName("student")
+            r[0].style.color="red"
+            
+            
+            
+        }
+    </script>
+
+</html>
