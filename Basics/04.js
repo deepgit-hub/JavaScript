e2f37d9985c47cc5ca14d664d4003ee43ee49742
Deepak <darkdeepak001@gmail.com>
2026-06-05 08:27:21 +0530

    Add rectangle area calculation in 04.js

--- Basics/04.js
+++ Basics/04.js
@@ -1,1 +1,20 @@
+/* 
+❓ Question 4: Rectangle Area
 
+👉 Create variables for:
+
+length
+width
+
+Find:
+
+area of rectangle
+
+Formula:
+
+area = length * width
+*/
+var length =2
+var width = 4
+var area = length*width
+console.log("Area of the Rectangle is "+area)
