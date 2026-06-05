ed4781a5558963b7b020fb0ec8d44432dea8c776
Deepak <darkdeepak001@gmail.com>
2026-06-05 08:24:44 +0530

    Update and rename 2.js to 02.js

---
+++ Basics/02.js
@@ -0,0 +1,21 @@
+/* ❓ Question 2: Addition of Two Numbers
+
+👉 Create:
+
+Two variables storing numbers
+Another variable to store their sum
+
+Print:
+
+First number
+Second number
+Result
+
+using:
+
+console.log() */
+
+var num1 = 10
+var num2 = 20
+var sum =num1+num2
+console.log(sum)
