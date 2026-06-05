2cde5bf003d339004459c0a08b87b780b6c47f9d
Deepak <darkdeepak001@gmail.com>
2026-06-05 08:28:47 +0530

    Fix CGPA value in student details
    Updated CGPA variable value to reflect correct data.

--- Basics/05.js
+++ Basics/05.js
@@ -1,1 +1,28 @@
+/*❓ Question 5: Student Details
 
+👉 Create variables for:
+
+Student name
+Department
+College
+CGPA
+
+Print output like:
+
+Student Name: Deepak
+Department: Computer Science
+College: Don Bosco College
+CGPA: 8.5
+
+using:
+
+console.log() */
+
+var name  = "Deepak"
+var dep ="Computer Science"
+var college = "Don Bosco College"
+var cgpa = 6
+console.log("Student Name : "+name)
+console.log("Department   : "+dep)
+console.log("College      : "+college)
+console.log("CGPA         : "+cgpa)
