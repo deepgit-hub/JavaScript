92a77446d5810f4de9718fb1be822899cef143c6
Deepak <darkdeepak001@gmail.com>
2026-06-08 13:09:12 +0530

    Add click event to change button text
    Implement button click event to change text.

--- EventListener/2.js
+++ EventListener/2.js
@@ -1,1 +1,18 @@
+🏆 Question 58
+Change Text on Click
+Click button
+↓
+Button text changes to:
+"I Was Clicked!"
 
+Expected:
+
+<button id="btn">Click Me</button>
+
+After click:
+
+I Was Clicked!
+
+Use:
+
+addEventListener("click", ...)
