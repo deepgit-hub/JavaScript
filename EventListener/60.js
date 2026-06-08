371d0997c4dccd31e91dfbd65b16360c747688cc
Deepak <darkdeepak001@gmail.com>
2026-06-08 13:39:38 +0530

    Add comment block for secret message functionality
    Added a comment block explaining the functionality of the code.

--- EventListener/60.js
+++ EventListener/60.js
@@ -1,3 +1,23 @@
-/**/
+/*
+🏆 Question 60 (Challenge)
+Secret Message
+
+Initially:
+
+Message Hidden
+
+Click button:
+
+Welcome Deepak 🚀
+
+Click again:
+
+Message Hidden
+
+Use:
+
+click
+textContent
+*/
 <h1 id="message">Message Hidden</h1>
 <button id="btn">revel</button>
