e6ddfe9f99e15f40748964d4e91c979a6c1cf452
Deepak <darkdeepak001@gmail.com>
2026-06-08 13:16:32 +0530

    Add mouseover effect to change heading color

--- EventListener/58.js
+++ EventListener/58.js
@@ -1,1 +1,18 @@
+🏆 Question 58
+Mouse Over Warning
+Mouse over heading
+↓
+Heading color becomes red
 
+Expected:
+
+<h1 id="title">Don't Touch Me</h1>
+
+When mouse enters:
+
+Color → Red
+
+Use:
+
+mouseover
+style.color
